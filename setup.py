059629ad07ccf8b46a41f351e17a035a15ac5f48
jmmshn <jmmshn@gmail.com>
2022-03-31 15:03:31 -0700

    wflows

--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
-# coding: utf-8
 # Copyright (c) Materials Virtual Lab
 # Distributed under the terms of the Modified BSD License.
 
-from setuptools import setup, find_namespace_packages
+from pathlib import Path
 
-import os
+from setuptools import find_namespace_packages, setup
 
-SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
+module_dir = Path(__file__).resolve().parent
 
-with open(os.path.join(SETUP_PTH, "README.md")) as f:
-    desc = f.read()
-
+with open(module_dir / "README.md") as f:
+    long_description = f.read()
 
 setup(
     name="pymatgen-analysis-defects",
     packages=find_namespace_packages(include=["pymatgen.analysis.*"]),
     version="0.0.1",
-    install_requires=["pymatgen>=2022.0.3"],
+    install_requires=["pymatgen>=2022.2.3"],
     extras_require={},
     package_data={},
-    author="materials virtual lab",
-    author_email="ongsp@eng.ucsd.edu",
-    maintainer="materials virtual lab",
+    author="Jimmy Shen",
+    author_email="jmmshn@gmail.com",
+    maintainer="Jimmy Shen",
     url="https://github.com/materialsproject/pymatgen-analysis-defects",
     license="BSD",
-    description="A template for creating add-ons for pymatgen.",
-    long_description=desc,
+    description="Add-ons to pymatgen for defect analysis.",
+    long_description=long_description,
     keywords=["pymatgen"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
