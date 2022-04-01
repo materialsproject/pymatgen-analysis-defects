6f5c4071e70051acc50f46a312cc7e34d79d2429
jmmshn <jmmshn@gmail.com>
2022-04-01 15:44:49 -0700

    ccd tests

---
+++ tests/test_ccd.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_ccd(test_dir):
+    from pymatgen.analysis.defects.ccd import ConfigurationCoordinateDiagram
+
+    ccd = ConfigurationCoordinateDiagram(
+        charge_gs=0,
+        charge_es=1,
+        dQ=1.0,
+        dE=2,
+        Q_gs=(-0.2, 0.0, 0.2),
+        Q_es=(0.8, 1.0, 1.2),
+        energies_es=(1.2, 1.0, 1.2),
+        energies_gs=(1.2, 1.0, 1.2),
+    )
+    assert ccd.omega_es == pytest.approx(ccd.omega_gs)
