2d23799ece25b03a2fe0ba776e0a6e30d1037e50
jmmshn <jmmshn@gmail.com>
2022-04-25 16:43:36 -0700

    small fixes and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from pymatgen.core import Structure
 
 
 @pytest.fixture(scope="session")
@@ -8,3 +9,8 @@ def test_dir():
     module_dir = Path(__file__).resolve().parent
     test_dir = module_dir / "test_files"
     return test_dir.resolve()
+
+
+@pytest.fixture(scope="session")
+def gan_struct(test_dir):
+    return Structure.from_file(test_dir / "GaN.vasp")
