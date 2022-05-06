d4f94bf5d19769253c5ad0ba0d3e981220540ee6
jmmshn <jmmshn@gmail.com>
2022-05-06 12:09:56 -0700

    more tests

---
+++ tests/test_generators.py
@@ -0,0 +1,17 @@
+from pymatgen.analysis.defect.core import Substitution, Vacancy
+from pymatgen.analysis.defect.generators import SubstitutionGenerator, VacancyGenerator
+
+
+def test_generators(gan_struct):
+    # Vacancy
+    vacancy_generator = VacancyGenerator(gan_struct)
+    for defect in vacancy_generator:
+        assert isinstance(defect, Vacancy)
+
+    # Substitution
+    sub_generator = SubstitutionGenerator(gan_struct, {"Ga": ["Mg", "Ca"]})
+    replaced_atoms = set()
+    for defect in sub_generator:
+        assert isinstance(defect, Substitution)
+        replaced_atoms.add(defect.site.specie.symbol)
+    assert replaced_atoms == {"Mg", "Ca"}
