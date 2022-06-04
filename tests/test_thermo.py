847c5b918b3d24d45b0ff8092d09b0bc34fb4beb
JSX <jmmshn@gmail.com>
2022-06-03 21:18:04 -0700

    envelope test

---
+++ tests/test_thermo.py
@@ -0,0 +1,11 @@
+from pymatgen.analysis.defect.thermo import get_lower_envelope, get_transitions
+
+
+def test_lower_envelope():
+    # Test the lower envelope and transition code with a simple example
+    lines = [[4, 12], [-1, 3], [-5, 4], [-2, 1], [3, 8], [-4, 14], [2, 12], [3, 8]]
+    lower_envelope_ref = [(4, 12), (3, 8), (-2, 1), (-5, 4)]  # answer from visual inspection (ordered)
+    transitions_ref = [(-4, -4), (-1.4, 3.8), (1, -1)]
+    lower_envelope = get_lower_envelope(lines)
+    assert lower_envelope == lower_envelope_ref
+    assert get_transitions(lower_envelope) == transitions_ref
