7afa0afe96bdcebd0ee099d878ceb7c1af1c0e93
JSX <jmmshn@gmail.com>
2022-07-12 11:50:07 -0700

    replacing pymatgen.analysis.defects

--- pymatgen/analysis/defects/__init__.py
+++ pymatgen/analysis/defects/__init__.py
@@ -1,3 +1,1 @@
-"""Addons to pymatgen.analysis.defects."""
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-from pymatgen.analysis.defects._version import __version__  # noqa: F401
+"""Module for defect analysis."""
