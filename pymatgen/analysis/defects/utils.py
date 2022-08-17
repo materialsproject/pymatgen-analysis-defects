7ff9924b7b483061b65f3415b049e0bb7d4d0066
JSX <jmmshn@gmail.com>
2022-08-16 20:39:57 -0700

    wip

--- pymatgen/analysis/defects/utils.py
+++ pymatgen/analysis/defects/utils.py
@@ -3,31 +3,37 @@ from __future__ import annotations
 
 import logging
 import math
+import operator
 from copy import deepcopy
 from pathlib import Path
+from typing import Generator
 
 import numpy as np
+from monty.dev import requires
 from monty.json import MSONable
+from numpy import typing as npt
 from numpy.linalg import norm
 from pymatgen.analysis.local_env import cn_opt_params
+from pymatgen.core import Lattice
+from pymatgen.io.vasp import VolumetricData
+from scipy.cluster.hierarchy import fcluster, linkage
+from scipy.spatial.distance import squareform
 
 try:
-    pass
+    from skimage.feature import peak_local_max
 
     peak_local_max_found = True
 except ImportError:
     peak_local_max_found = False
 
 __author__ = (
-    "Danny Broberg, Shyam Dwaraknath, Bharat Medasani, Nils Zimmermann, "
+    "Jimmy-Xuan Shen, Danny Broberg, Shyam Dwaraknath, Bharat Medasani, Nils Zimmermann, "
     "Geoffroy Hautier"
 )
-__copyright__ = "Copyright 2014, The Materials Project"
-__version__ = "1.0"
-__maintainer__ = "Danny Broberg, Shyam Dwaraknath"
-__email__ = "dbroberg@berkeley.edu, shyamd@lbl.gov"
+__maintainer__ = "Jimmy-Xuan Shen"
+__email__ = "jmmshn@gmail.com"
 __status__ = "Development"
-__date__ = "January 11, 2018"
+__date__ = "Aug 15, 2022"
 
 logger = logging.getLogger(__name__)
 hart_to_ev = 27.2114
@@ -112,7 +118,7 @@ def eV_to_k(energy):
     return math.sqrt(energy / invang_to_ev) * ang_to_bohr
 
 
-def genrecip(a1, a2, a3, encut):
+def genrecip(a1, a2, a3, encut) -> Generator[npt.ArrayLike, None, None]:
     """Generate reciprocal lattice vectors within the energy cutoff.
 
     Args:
@@ -120,6 +126,7 @@ def genrecip(a1, a2, a3, encut):
         a2: Lattice vector b (in Bohrs)
         a3: Lattice vector c (in Bohrs)
         encut: energy cut off in eV
+
     Returns:
         reciprocal lattice vectors with energy less than encut
     """
@@ -221,3 +228,148 @@ def get_zfile(
         return None
 
     raise FileNotFoundError(f"Could not find {base_name} or {base_name}.gz file.")
+
+
+def generic_groupby(list_in, comp=operator.eq):
+    """
+    Group a list of unsortable objects
+    Args:
+        list_in: A list of generic objects
+        comp: (Default value = operator.eq) The comparator
+    Returns:
+        [int] list of labels for the input list
+    """
+    list_out = [None] * len(list_in)
+    label_num = 0
+    for i1, ls1 in enumerate(list_out):
+        if ls1 is not None:
+            continue
+        list_out[i1] = label_num
+        for i2, ls2 in list(enumerate(list_out))[(i1 + 1) :]:
+            if comp(list_in[i1], list_in[i2]):
+                if ls2 is None:
+                    list_out[i2] = list_out[i1]
+                else:
+                    list_out[i1] = ls2
+                    label_num -= 1
+        label_num += 1
+    return list_out
+
+
+@requires(
+    peak_local_max_found,
+    "get_local_extrema requires skimage.feature.peak_local_max module"
+    " to be installed. Please confirm your skimage installation.",
+)
+def get_local_extrema(chgcar: VolumetricData, find_min: bool = True) -> npt.NDArray:
+    """
+    Get all local extrema fractional coordinates in charge density,
+    searching for local minimum by default. Note that sites are NOT grouped
+    symmetrically.
+
+    Args:
+        find_min (bool): True to find local minimum else maximum, otherwise
+            find local maximum.
+
+    Returns:
+        extrema_coords (list): list of fractional coordinates corresponding
+            to local extrema.
+    """
+
+    if find_min:
+        sign = -1
+    else:
+        sign = 1
+
+    # Make 3x3x3 supercell
+    # This is a trick to resolve the periodical boundary issue.
+    # TODO: Add code to pyrho for max and min filtering.
+    total_chg = sign * chgcar.data["total"]
+    total_chg = np.tile(total_chg, reps=(3, 3, 3))
+    coordinates = peak_local_max(total_chg, min_distance=1)
+
+    # Remove duplicated sites introduced by supercell.
+    f_coords = [coord / total_chg.shape * 3 for coord in coordinates]
+    f_coords = [
+        f - 1 for f in f_coords if all(np.array(f) < 2) and all(np.array(f) >= 1)
+    ]
+
+    return np.array(f_coords)
+
+
+def cluster_nodes(
+    vf_coords: npt.ArrayLike, lattice: Lattice, tol: float = 0.2
+) -> npt.NDArray:
+    """
+    Cluster nodes that are too close together using a tol.
+
+    Args:
+        tol (float): A distance tolerance. PBC is taken into account.
+    """
+    # Manually generate the distance matrix (which needs to take into
+    # account PBC.
+    dist_matrix = np.array(lattice.get_all_distances(vf_coords, vf_coords))
+    dist_matrix = (dist_matrix + dist_matrix.T) / 2
+
+    for i in range(len(dist_matrix)):
+        dist_matrix[i, i] = 0
+    condensed_m = squareform(dist_matrix)
+    z = linkage(condensed_m)
+    cn = fcluster(z, tol, criterion="distance")
+    merged_fcoords = []
+
+    for n in set(cn):
+        frac_coords = []
+        for i, j in enumerate(np.where(cn == n)[0]):
+            if i == 0:
+                frac_coords.append(vf_coords[j])
+            else:
+                f_coords = vf_coords[j]
+                # We need the image to combine the frac_coords properly.
+                d, image = lattice.get_distance_and_image(frac_coords[0], f_coords)
+                frac_coords.append(f_coords + image)
+        merged_fcoords.append(np.average(frac_coords, axis=0))
+
+    merged_fcoords = [f - np.floor(f) for f in merged_fcoords]
+    merged_fcoords = [f * (np.abs(f - 1) > 1e-15) for f in merged_fcoords]
+    # the second line for fringe cases like
+    # np.array([ 5.0000000e-01 -4.4408921e-17  5.0000000e-01])
+    # where the shift to [0,1) does not work due to float precision
+
+    return np.array(merged_fcoords)
+
+
+def get_avg_chg(
+    chgcar: VolumetricData, fcoord: npt.NDArray, radius: float = 0.4
+) -> float:
+    """Get the average charge in a sphere.
+
+    Args:
+        chgcar: The charge density.
+        fcoord: The fractional coordinates of the center of the sphere.
+        radius: The radius of the sphere in Angstroms.
+
+    Returns:
+        The average charge in the sphere.
+
+
+    """
+
+    def _dist_mat(pos_frac):
+        # return a matrix that contains the distances
+        aa = np.linspace(0, 1, len(chgcar.get_axis_grid(0)), endpoint=False)
+        bb = np.linspace(0, 1, len(chgcar.get_axis_grid(1)), endpoint=False)
+        cc = np.linspace(0, 1, len(chgcar.get_axis_grid(2)), endpoint=False)
+        AA, BB, CC = np.meshgrid(aa, bb, cc, indexing="ij")
+        dist_from_pos = chgcar.structure.lattice.get_all_distances(
+            fcoords1=np.vstack([AA.flatten(), BB.flatten(), CC.flatten()]).T,
+            fcoords2=pos_frac,
+        )
+        return dist_from_pos.reshape(AA.shape)
+
+    if np.any(fcoord < 0) or np.any(fcoord > 1):
+        raise ValueError("f_coords must be in [0,1)")
+    mask = _dist_mat(fcoord) < radius
+    vol_sphere = chgcar.structure.volume * (mask.sum() / chgcar.ngridpts)
+    chg_in_sphere = np.sum(chgcar.data["total"] * mask) / mask.size / vol_sphere
+    return chg_in_sphere
