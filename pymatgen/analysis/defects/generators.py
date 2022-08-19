070df2a1084904d3341ba9d5307a3b081219cd0b
@jmmshn <jmmshn@gmail.com>
2022-08-19 12:11:31 -0700

    WIP interstitial generator

--- pymatgen/analysis/defects/generators.py
+++ pymatgen/analysis/defects/generators.py
@@ -6,9 +6,11 @@ from typing import Iterable
 
 from monty.json import MSONable
 from pymatgen.core import Element, PeriodicSite, Species, Structure
+from pymatgen.io.vasp.outputs import Chgcar
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
-from pymatgen.analysis.defects.core import Defect, Substitution, Vacancy
+from pymatgen.analysis.defects.core import Defect, Interstitial, Substitution, Vacancy
+from pymatgen.analysis.defects.utils import ChargeInsertionAnalyzer
 
 __author__ = "Jimmy-Xuan Shen"
 __copyright__ = "Copyright 2022, The Materials Project"
@@ -45,18 +47,16 @@ class DefectGenerator(MSONable, metaclass=ABCMeta):
 class VacancyGenerator(DefectGenerator):
     """Generate vacancy for each symmetry distinct site in a structure."""
 
-    def __init__(self, structure: Structure, rm_species: list[str] | None = None):
+    def __init__(self, structure: Structure, rm_species: str | Species = None):
         """Initialize a vacancy generator.
 
         Args:
             structure: The bulk structure the vacancies are generated from.
-            rm_species: The species to be removed.
+            rm_species: The species to be removed. If None considered all species.
         """
         super().__init__(structure)
         all_species = [*map(str, structure.composition.elements)]
-        self.rm_species = (
-            [*map(str, rm_species)] if rm_species is not None else all_species
-        )
+        self.rm_species = str(rm_species) if rm_species else all_species
 
     def generate_defects(self) -> Iterable[Defect]:
         """Generate the vacancy objects."""
@@ -99,6 +99,75 @@ class SubstitutionGenerator(DefectGenerator):
                 properties=site.properties,
             )
             yield Substitution(self.structure, sub_site)
+
+
+class InterstitialGenerator(DefectGenerator):
+    """Generate intersitials defects in structure."""
+
+    def __init__(
+        self,
+        structure: Structure,
+        insertions: dict[str | Species | Element, list[list[float]]],
+    ):
+        """Initialize the interstitial generator.
+
+        Args:
+            structure: The bulk structure the interstitials atoms are placed in.
+            insertions: Insertions in the form of a dictionary.
+                e.g. ``{"Mg": [[0.5, 0.5, 0.5], [0.25, 0.25, 0.25]]}`` means that we want to generate
+                two interstitials at [0.5, 0.5, 0.5] and [0.25, 0.25, 0.25].
+
+        """
+        super().__init__(structure)
+        self.insertions = insertions
+
+    @classmethod
+    def from_chgcar(
+        cls,
+        chgcar: Chgcar,
+        i_species: str | Species | Element,
+        avg_radius: float = 0.4,
+        max_avg_charge: float = 1.0,
+        n_groups: int = None,
+        **kwargs,
+    ) -> InterstitialGenerator:
+        """Generate interstitials from a CHGCAR object.
+
+        Identify the interstitial sites from the CHGCAR object using ``ChargeInsertionAnalyzer``.
+        ``ChargeInsertionAnalyzer`` examines the local minmina in the charge density and organizes
+        them into symmetry equivalent groups.
+        The groups are ranked by the average charge density in ``avg_radius`` around each site,
+        we will filter out groups with average charge density higher than ``max_avg_charge``.
+        If ``n_groups`` is specified, we will only select the ``n_groups`` groups with the lowest
+        average charge density.
+
+        Args:
+            chgcar: The CHGCAR object.
+            insertions: Insertions in the form of a dictionary.
+                e.g. ``{"Mg": [[0.5, 0.5, 0.5], [0.25, 0.25, 0.25]]}`` means that we want to generate
+                two interstitials at [0.5, 0.5, 0.5] and [0.25, 0.25, 0.25].
+            avg_radius: The radius used to calculate average charge density.
+            max_avg_charge: Do no consider local minmas with avg charge above this value.
+            n_groups: Only consider the n_groups with lowest average charge density.
+            If None, all groups are considered.
+            **kwargs: Other keyword arguments passed to ``ChargeInsertionAnalyzer``.
+
+        Returns:
+            An ``InterstitialGenerator`` object.
+        """
+        cia = ChargeInsertionAnalyzer(chgcar, **kwargs)
+        insert_groups = cia.filter_and_group(
+            avg_radius=avg_radius, max_radius=max_avg_charge
+        )
+        i_pos = [group[0] for _, group in insert_groups]
+        if n_groups is not None:
+            i_pos = i_pos[:n_groups]
+        return cls(cia.chgcar.structure.copy(), insertions={i_species: i_pos})
+
+    def generate_defects(self) -> Iterable[Defect]:
+        """Generate the interstitial objects."""
+        for site in self.i_sites:
+            yield Interstitial(self.structure, site)
 
 
 def element_str(sp_or_el: Species | Element) -> str:
