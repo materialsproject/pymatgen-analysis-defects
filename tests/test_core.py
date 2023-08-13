e50967150ba689f07ddd4b13910aead494a5dc4e
Seán Kavanagh <51478689+kavanase@users.noreply.github.com>
2023-08-13 05:36:26 +0100

    Defect generation updates (#133)
    * Pass `VoronoiInterstitialGenerator` parameters through to `TopographyAnalyzer` (bugfix)
    
    * Pass `min_dist` from `VoronoiInterstitialGenerator` initialisation through to `InterstitialGenerator` superclass (bugfix)
    
    * Refactor `filter_colliding` to return coords _and_ index, for correct multiplicities (bugfix)
    
    * Refactor `multiplicies` to `multiplicities` (small typo)
    
    * Update type hint
    
    * Add `equivalent_sites` parameter to `Defect` objects and `target_frac_coords` parameter to `get_supercell_structure` to allow choice of generated defect site in supercell
    
    * Add `return_site` option to `get_supercell_structure` (useful information!)
    
    * Update supercell site setting for dummy species as well
    
    * Automatically bump `max_cell_range` to 2 in `TopographyAnalyzer` if `num_atoms < 5`, for accurate/complete Voronoi tessellation (small unit cells anyway so negligible cost!)
    
    * Use `oxi_state` of substituting specie in the host structure if present (when guessing oxi state of substitution defect) – i.e. antisite defects!
    
    * Add `target_frac_coords` and `return_site` to `DefectComplex` `get_supercell_structure` to make compatible with supertype `Defect`
    
    * Linting
    
    * Comment cleanup
    
    * Also return list of equivalent supercell sites if `return_sites` set to `True`
    
    * Use self.site if self.equivalent_sites is empty
    
    * Make Defect.equivalent_sites match element symbol for Defect.site (rather than Defect.defect_site) with `Substitution` defects
    
    * Fix typo
    
    * Remove oxidation states from defect supercell sites (as well as overall returned structure)
    
    * Add tests for `target_frac_coords` with `get_supercell_structure` for `Interstitial` and `Substitution`
    
    * Add test for oxidation state guessing with (antisite) substitution defects

--- tests/test_core.py
+++ tests/test_core.py
@@ -79,6 +79,24 @@ def test_substitution(gan_struct):
     sub_ = Substitution.from_dict(dd)
     assert sub_.get_charge_states() == [-1, 0, 1, 2]
 
+    # test target_frac_coords with get_supercell_structure
+    sub_sc_struct = sub.get_supercell_structure()
+    finder = DefectSiteFinder()
+    fpos = finder.get_defect_fpos(sub_sc_struct, sub.structure)
+    assert np.allclose(fpos, [0.1250, 0.0833335, 0.18794])
+    # change target coords:
+    sub_sc_struct = sub.get_supercell_structure(target_frac_coords=[0.3, 0.5, 0.9])
+    fpos = finder.get_defect_fpos(sub_sc_struct, sub.structure)
+    assert np.allclose(fpos, [0.375, 0.5833335, 0.68794])  # closest equivalent site
+
+    # test oxidation state setting for substitutional defects when substitution is an antisite:
+    # from pymatgen.analysis.defects.generators import AntiSiteGenerator
+    ga_site = s.sites[0]
+    assert ga_site.specie.symbol == "Ga"
+    n_site = PeriodicSite(Specie("N"), ga_site.frac_coords, s.lattice)
+    n_ga = Substitution(s, n_site)
+    assert n_ga.get_charge_states() == [-7, -6, -5, -4, -3, -2, -1, 0, 1]
+
 
 def test_interstitial(gan_struct):
     s = gan_struct.copy()
@@ -93,6 +111,15 @@ def test_interstitial(gan_struct):
     assert inter.name == "N_i"
     assert str(inter) == "N intersitial site at [0.00,0.00,0.75]"
     assert inter.element_changes == {Element("N"): 1}
+
+    # test target_frac_coords with get_supercell_structure
+    finder = DefectSiteFinder()
+    fpos = finder.get_defect_fpos(sc, inter.structure)
+    assert np.allclose(fpos, [0, 0, 0.398096581])
+    # change target coords:
+    inter_sc_struct = inter.get_supercell_structure(target_frac_coords=[0.3, 0.5, 0.9])
+    fpos = finder.get_defect_fpos(inter_sc_struct, inter.structure)
+    assert np.allclose(fpos, [0.25, 0.5, 0.89809658])  # closest equivalent site
 
 
 def test_adsorbate(gan_struct):
